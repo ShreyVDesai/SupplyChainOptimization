4a8b99e0570605f28eecca26a178634defdc0a38
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-19 04:09:50 -0400

    Testing deployment using terraform-10

--- terraform/compute_image.tf
+++ terraform/compute_image.tf
@@ -1,5 +1,5 @@
 resource "google_compute_image" "airflow_image" {
   name        = "my-airflow-image"
-  source_disk = google_compute_instance.airflow_vm.self_link
+  source_disk = "projects/${var.project_id}/zones/${var.zone}/disks/${google_compute_instance.airflow_vm.boot_disk[0].device_name}"
   depends_on  = [google_compute_instance.airflow_vm]
 }
