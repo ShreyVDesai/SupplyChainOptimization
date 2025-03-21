436e4e69e022cbad3e7023ad15c29b5e3d8a8043
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-21 18:00:00 -0400

    Testing image infra setup-23

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,10 @@
 output "ssh_private_key" {
-  value     = tls_private_key.ssh_key.private_key_pem
+  value     = tls_private_key.main.private_key_pem
   sensitive = true
 }
 
 output "ssh_public_key" {
-  value = tls_private_key.ssh_key.public_key_openssh
+  value = tls_private_key.main.public_key_openssh
 }
 
 output "vm_external_ip" {
