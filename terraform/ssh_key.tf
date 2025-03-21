68e99f210810c17878b92c3545a1a0f9ee2bda74
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-21 18:02:54 -0400

    Testing image infra setup-23

--- terraform/ssh_key.tf
+++ terraform/ssh_key.tf
@@ -9,11 +9,3 @@ resource "tls_private_key" "main" {
   rsa_bits  = 4096
 }
 
-output "ssh_private_key" {
-  value     = tls_private_key.main.private_key_pem
-  sensitive = true
-}
-
-output "ssh_public_key" {
-  value = "ubuntu:${tls_private_key.main.public_key_openssh}"
-}
