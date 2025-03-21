f886804f2543502f3e26acc314a8055271d0ff7f
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-21 16:23:21 -0400

    Testing image infra setup-22

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,10 @@ output "ssh_private_key" {
   sensitive = true
 }
 
+output "ssh_public_key" {
+  value = tls_private_key.ssh_key.public_key_openssh
+}
+
 output "vm_external_ip" {
   value = google_compute_instance.airflow_vm.network_interface[0].access_config[0].nat_ip
 }
